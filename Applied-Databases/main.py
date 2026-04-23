bf776f9f096a3b77a63db83f11933eb6e9778912
Cathal <cathalred88@hotmail.com>
2026-04-23 21:58:03 +0100

    got main menu selection fucntion working

--- Applied-Databases/main.py
+++ Applied-Databases/main.py
@@ -3,3 +3,82 @@
 # Date: 2024-04-21
 # Description: This is the main file for the Applied Databases project. It will contain the main code for the project.
 
+# imports
+
+# write a main menu for the user to interact with the database
+def main_menu():
+    print("Conference Management")
+    print("---------------------\n")
+    print("MENU" )
+    print("====")
+    print("1 - View Speakers & Sessions")
+    print("2 - View Attendees by Company")
+    print("3 - Add New Attendee")
+    print("4 - View Connected Attendees")
+    print("5 - Add Attendee Connection")
+    print("6 - View Rooms")
+    print("X - Exit Application")
+
+    choice = input("Choice: ")
+
+    if choice == "1":
+        ViewSpeakersAndSessions()
+    elif choice == "2":
+        ViewAttendeesByCompany()
+    elif choice == "3":
+        AddNewAttendee()
+    elif choice == "4":
+        ViewConnectedAttendees()
+    elif choice == "5":
+        AddAttendeeConnection()
+    elif choice == "6":
+        ViewRooms()
+    elif choice == "X" or choice == "x":
+        print("Goodbye!")
+        exit()
+    else:
+        print("Invalid choice. Please try again.")
+        main_menu()
+
+# functions for each menu option
+def ViewSpeakersAndSessions():
+    print("View Speakers & Sessions")
+    # code to view speakers and sessions from stored database
+    speaker = input("Enter speaker name: ")
+    # ... (code to fetch and display speaker information)
+    main_menu()
+
+def ViewAttendeesByCompany():
+    print("View Attendees by Company")
+    # code to view attendees by company
+    company = input("Enter company name: ")
+    # ... (code to fetch and display attendees for the specified company)
+    main_menu()
+
+def AddNewAttendee():
+    print("Add New Attendee")
+    # code to add new attendee
+    main_menu()
+
+def ViewConnectedAttendees():
+    print("View Connected Attendees")
+    # code to view connected attendees
+    main_menu()
+
+def AddAttendeeConnection():
+    print("Add Attendee Connection")
+    # code to add attendee connection
+    main_menu() 
+
+def ViewRooms():
+    print("View Rooms")
+    # code to view rooms
+
+    main_menu()
+
+
+# call the main menu function
+if __name__ == "__main__":
+    main_menu()
+
+# End of main.py
